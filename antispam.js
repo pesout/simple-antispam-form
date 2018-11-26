5c47fc34437a321c0636bdae254424e5ec519e51
Štěpán Pešout <stepan@pesout.eu>
2018-11-26 11:54:32 +0100

    Add JS script

---
+++ antispam.js
@@ -0,0 +1,34 @@
+/*
+************************
+Created by Stepan Pesout
+*****www.pesout.eu******
+*****@stepanpesout******
+************************
+*/
+
+
+MAX = 50;
+
+var numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine", "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", "seventy", "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine", "eighty", "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine", "ninety", "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine"];
+
+var first, second;
+
+function generateNew() {
+    if (MAX > 99) MAX = 99;
+
+    first  = Math.floor(Math.random() * (MAX+1));
+    second = Math.floor(Math.random() * (MAX+1));
+
+    document.getElementById('first').innerHTML  = numbers[first];
+    document.getElementById('second').innerHTML = numbers[second];
+}
+
+function resultCheck(res) {
+    if (first+second == res) alert("correct");
+    else {
+        alert ("not correct");
+        generateNew();
+    }
+}
+
+generateNew();
