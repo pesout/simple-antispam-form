c0c8b595eae50ea1028ecdf73b6c1190d64b5f2c
Štěpán Pešout <stepan@pesout.eu>
2018-11-26 11:57:29 +0100

    Update antispam.js, add user functions

--- antispam.js
+++ antispam.js
@@ -7,7 +7,18 @@ Created by Stepan Pesout
 */
 
 
-MAX = 50;
+function isCorrect () { // Code, if success
+    alert("success");
+}
+
+function isNotCorrect () { // Code, if not success
+    alert ("not success");
+    generateNew(); // Generates new antispam
+}
+
+MAX = 50; // Maximal values in antispam, must be less then 100
+
+/*-----------------------------------------------------------*/
 
 var numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty", "thirty-one", "thirty-two", "thirty-three", "thirty-four", "thirty-five", "thirty-six", "thirty-seven", "thirty-eight", "thirty-nine", "forty", "forty-one", "forty-two", "forty-three", "forty-four", "forty-five", "forty-six", "forty-seven", "forty-eight", "forty-nine", "fifty", "fifty-one", "fifty-two", "fifty-three", "fifty-four", "fifty-five", "fifty-six", "fifty-seven", "fifty-eight", "fifty-nine", "sixty", "sixty-one", "sixty-two", "sixty-three", "sixty-four", "sixty-five", "sixty-six", "sixty-seven", "sixty-eight", "sixty-nine", "seventy", "seventy-one", "seventy-two", "seventy-three", "seventy-four", "seventy-five", "seventy-six", "seventy-seven", "seventy-eight", "seventy-nine", "eighty", "eighty-one", "eighty-two", "eighty-three", "eighty-four", "eighty-five", "eighty-six", "eighty-seven", "eighty-eight", "eighty-nine", "ninety", "ninety-one", "ninety-two", "ninety-three", "ninety-four", "ninety-five", "ninety-six", "ninety-seven", "ninety-eight", "ninety-nine"];
 
@@ -24,11 +35,8 @@ function generateNew() {
 }
 
 function resultCheck(res) {
-    if (first+second == res) alert("correct");
-    else {
-        alert ("not correct");
-        generateNew();
-    }
+    if (first+second == res) isCorrect();
+    else isNotCorrect();
 }
 
 generateNew();
